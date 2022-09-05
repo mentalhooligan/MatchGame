169e430a967dd919a1c8c70014711d8f7c2eb1c0
Ivan <untro999@yandex.ru>
2022-09-05 12:21:08 +0300

    added mousedown events, method for hiding clicked elements

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -15,16 +15,33 @@ using System.Windows.Shapes;
 
 namespace MatchGame
 {
+    using System.Windows.Threading;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
         public MainWindow()
         {
             InitializeComponent();
 
+            timer.Interval = TimeSpan.FromSeconds(0.1);
+            timer.Tick += Timer_Tick; 
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed/10F).ToString("0.0");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + "ur stupid haha";
+            }
         }
 
         private void SetUpGame()
@@ -44,10 +61,49 @@ namespace MatchGame
 
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) // for each textblock in maingrid does this
             {
-                int index = random.Next(animalEmoji.Count); //assigns a random number from 0 to amount of left emojis in the emoji list
-                string nextEmoji = animalEmoji[index]; // gets a random emoji
-                textBlock.Text = nextEmoji; // puts that random emoji into the block in the window
-                animalEmoji.RemoveAt(index); // removes used emoji from the list of emojis
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalEmoji.Count); //assigns a random number from 0 to amount of left emojis in the emoji list
+                    string nextEmoji = animalEmoji[index]; // gets a random emoji
+                    textBlock.Text = nextEmoji; // puts that random emoji into the block in the window
+                    animalEmoji.RemoveAt(index); // removes used emoji from the list of emojis
+                }
+            }
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                matchesFound++;
+                findingMatch = false;
+                textBlock.Visibility = Visibility.Hidden;
+
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound==8)
+            {   
+                SetUpGame();
             }
         }
     }
